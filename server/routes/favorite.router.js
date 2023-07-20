bb69373467daee119ed52a738a13258c40fd7a0a
Savon <savonstephens4@gmail.com>
2023-07-20 12:29:21 -0700

    get and post server routes

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,11 +5,37 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
+  //return favorite images 
+  console.log('in /api/favorite');
+  const queryText = `SELECT * FROM gif ORDER BY name;`
+  pool.query(queryText)
+  .then((results) => {
+    res.send(results.rows)
+  })
+  .catch((error) => {
+    console.log(`error in query${error}`);
+    res.sendStatus(500)
+  })
   res.sendStatus(200);
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
+  console.log('inside of /api/favorite post, req.body' , req.body);
+  let url = req.body
+  //query text for data fields and sql injection 
+  const queryText = `INSERT INTO "gif" (path)
+  VALUES($1)`
+  //redeclaring data fields 
+  const queryParams = [url]
+  //bringing in pool 
+  pool.query(queryText, queryParams)
+    .then((results) => {
+      res.send(201)
+    }).catch((error) => {
+      console.log(`error making query ${queryText}`, error);
+      res.sendStatus(500)
+    })
   res.sendStatus(200);
 });
 
