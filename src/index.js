72a3a65064a8a8ca235ebb79d37f07d05db35845
Peder Goodman <peder.goodman@gmail.com>
2023-07-20 13:55:26 -0500

    saga & redux boilerplate

--- src/index.js
+++ src/index.js
@@ -2,9 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects'
+import axios from 'axios';
+
+
+
+// Saga root generator function
+function* rootSaga() {
+
+
+}; // end rootSaga
+
+
+// SAGAS
+
+
+
+
+// REDUCERS
+
+
+
+
+
+// COMBINE THEM ALL
+
+const sagaMiddleware = createSagaMiddleware();
+
+
+
+const store = createStore(
+    combineReducers({
+        // lists
+    }),
+    applyMiddleware(logger, sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 );
